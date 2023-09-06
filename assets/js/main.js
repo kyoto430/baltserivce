f5c24b3d8a9b9282aa69e9c998e5a64acce618ad
Kyoto430 <nateriver345@gmail.com>
2023-09-06 16:57:16 +0300

    feat: add about, slider, footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 console.log('init project');
+
+new Swiper('.about__slider', {
+  slidesPerView: 1,
+  loop: true,
+  navigation: {
+    prevEl: '.slider__btn-prev',
+    nextEl: '.slider__btn-next',
+  },
+})
