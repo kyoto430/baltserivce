ebfa33a57237844b8fde9856f03b07a8a47d7de7
Kyoto430 <nateriver345@gmail.com>
2023-09-11 16:59:30 +0300

    feat: add adaptive for index page

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,38 @@ new Swiper('.photo__slider', {
     nextEl: '.slider__btn-next',
   },
 })
+
+// Меню бургер
+function burger() {
+  const burgerBtn = document.querySelector('.menu__burger');
+  const menu = document.querySelector('.header__nav');
+  const menuLinks = document.querySelectorAll('.menu__link');
+  const overlay = document.querySelector('.overlay');
+
+  burgerBtn.addEventListener('click', function () {
+    document.body.classList.toggle('lock');
+    burgerBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+    overlay.classList.toggle('active');
+  });
+
+  menuLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      document.body.classList.remove('lock');
+      burgerBtn.classList.remove('active');
+      menu.classList.remove('active');
+      overlay.classList.remove('active');
+    });
+  });
+
+  document.addEventListener('click', function (event) {
+    if (!menu.contains(event.target) && !burgerBtn.contains(event.target)) {
+      document.body.classList.remove('lock');
+      burgerBtn.classList.remove('active');
+      menu.classList.remove('active');
+      overlay.classList.remove('active');
+    }
+  });
+}
+
+burger();
