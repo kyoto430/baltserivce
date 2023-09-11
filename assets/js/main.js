90a52ade60ff441bfceec4bbd9f26252c16618de
Kyoto430 <nateriver345@gmail.com>
2023-09-11 23:19:48 +0300

    refactor: add slider js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,6 @@
-console.log('init project');
-
-new Swiper('.about__slider', {
-  slidesPerView: 1,
-  loop: false,
-  navigation: {
-    prevEl: '.slider__btn-prev',
-    nextEl: '.slider__btn-next',
-  },
-})
-
-new Swiper('.photo__slider', {
-  slidesPerView: 4,
-  slidesPerGroup: 2,
-  spaceBetween: 20,
-  loop: false,
-  navigation: {
-    prevEl: '.slider__btn-prev',
-    nextEl: '.slider__btn-next',
-  },
-})
-
 // Меню бургер
 function burger() {
+  console.log('burger');
   const burgerBtn = document.querySelector('.menu__burger');
   const menu = document.querySelector('.header__nav');
   const menuLinks = document.querySelectorAll('.menu__link');
---
+++ assets/js/slider.js
@@ -0,0 +1,21 @@
+console.log('init project');
+
+new Swiper('.about__slider', {
+  slidesPerView: 1,
+  loop: false,
+  navigation: {
+    prevEl: '.slider__btn-prev',
+    nextEl: '.slider__btn-next',
+  },
+})
+
+new Swiper('.photo__slider', {
+  slidesPerView: 4,
+  slidesPerGroup: 2,
+  spaceBetween: 20,
+  loop: false,
+  navigation: {
+    prevEl: '.slider__btn-prev',
+    nextEl: '.slider__btn-next',
+  },
+})
