17239ebad75711c4b4a4cf6ba6225a93c1724adf
Kyoto430 <nateriver345@gmail.com>
2023-09-12 14:14:50 +0300

    fix: update adaptive

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -7,15 +7,39 @@ new Swiper('.about__slider', {
     prevEl: '.slider__btn-prev',
     nextEl: '.slider__btn-next',
   },
-})
+});
 
-new Swiper('.photo__slider', {
-  slidesPerView: 4,
-  slidesPerGroup: 2,
-  spaceBetween: 20,
-  loop: false,
-  navigation: {
-    prevEl: '.slider__btn-prev',
-    nextEl: '.slider__btn-next',
-  },
-})
+if (window.innerWidth > 768) {
+  new Swiper('.photo__slider', {
+    slidesPerView: 4,
+    slidesPerGroup: 2,
+    spaceBetween: 20,
+    loop: false,
+    navigation: {
+      prevEl: '.slider__btn-prev',
+      nextEl: '.slider__btn-next',
+    },
+  });
+} else if (window.innerWidth <= 768 && window.innerWidth >= 375) {
+  new Swiper('.photo__slider', {
+    slidesPerView: 2.5,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    loop: false,
+    navigation: {
+      prevEl: '.slider__btn-prev',
+      nextEl: '.slider__btn-next',
+    },
+  });
+} else if (window.innerWidth <= 375) {
+  new Swiper('.photo__slider', {
+    slidesPerView: 1.25,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    loop: false,
+    navigation: {
+      prevEl: '.slider__btn-prev',
+      nextEl: '.slider__btn-next',
+    },
+  });
+}
